fcab2d6d0bcf1a23df4846cf8685b1051db4c63f
yotamin <yotamin@post.bgu.ac.il>
2019-02-11 11:53:31 +0200

    keras GAN and DCGAN

---
+++ Keras/GAN.py
@@ -0,0 +1,111 @@
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Reshape
+from keras.optimizers import Adam
+from keras.datasets import mnist
+from utils import dataIterator, sample_images
+from tqdm import tqdm
+
+
+class GAN:
+
+    def __init__(self, discriminator_layers_dims,
+                 generator_layers_dims,
+                 x_input_shape=(28, 28, 1),
+                 g_latent_dim=100,
+                 discriminator_optimizer=Adam(0.0001),
+                 generator_optimizer=Adam(0.0001)):
+
+        self.x_input_shape = x_input_shape
+        self.discriminator_layers_dims = discriminator_layers_dims
+        self.generator_layers_dims = generator_layers_dims
+        self.g_latent_dim = g_latent_dim
+
+        self._discriminator = self.build_discriminator()
+        self._discriminator.compile(optimizer=discriminator_optimizer, loss='binary_crossentropy')
+
+        # Gradients that will pass through the combined model will not update the discriminator
+        self._discriminator.trainable = False
+        for layer in self._discriminator.layers:
+            layer.trainable = False
+
+        self._generator = self.build_generator()
+
+        # Create the combined GAN model
+        z = Input((self.g_latent_dim,))
+        generator_out = self._generator(z)
+        discriminator_out = self._discriminator(generator_out)
+        self._combined_model = Model(inputs=z, outputs=discriminator_out)
+        self._combined_model.compile(optimizer=generator_optimizer, loss='binary_crossentropy')
+
+    def build_discriminator(self):
+        input = Input(self.x_input_shape)
+        x = Flatten()(input)
+        for layer_dim in self.discriminator_layers_dims:
+            # Regularization can be added
+            x = Dense(units=layer_dim, activation='relu')(x)
+        output = Dense(units=1, activation='sigmoid')(x)
+        return Model(inputs=input, outputs=output)
+
+    def build_generator(self):
+        input = Input((self.g_latent_dim,))
+        x = input
+        for layer_dim in self.generator_layers_dims[:-1]:
+            # Regularization can be added
+            x = Dense(units=layer_dim, activation='relu')(x)
+        x = Dense(units=self.generator_layers_dims[-1], activation='tanh')(x)
+        output = Reshape(self.x_input_shape)(x)
+        return Model(inputs=input, outputs=output)
+
+    def train_on_batch(self, x,
+                       z):
+
+        if len(x) != len(z):
+            raise ValueError('x and z must have the same batch size')
+
+        # -----------------------
+        # Train the discriminator
+
+        # Train the discriminator to mark real data with 1's
+        d_real_loss = self._discriminator.train_on_batch(x, np.ones(len(x)))
+
+        # Train the discriminator to mark generated data with 0's
+        d_generated_loss = self._discriminator.train_on_batch(self._generator.predict(z), np.zeros(len(z)))
+
+        d_loss = 0.5 * (d_real_loss + d_generated_loss)
+
+        # -----------------------
+        # Train the generator
+        g_loss = self._combined_model.train_on_batch(z, np.ones(len(z)))
+
+        return d_loss, g_loss
+
+    def fit(self, x,
+            batch_size=32,
+            epochs=1,
+            verbose=True):
+
+        data_iterator = dataIterator(x, batch_size)
+        batches_per_epoch = len(x) // batch_size
+
+        batches_per_epoch += 1 if batches_per_epoch < 1 else 0
+
+        for epoch in range(1, epochs+1):
+            tqdm_ = tqdm(range(batches_per_epoch), disable=not verbose)
+            for _ in tqdm_:
+                x_current_batch = next(data_iterator)
+                z = np.random.normal(0, 1, (batch_size, self.g_latent_dim))
+                d_loss, g_loss = self.train_on_batch(x_current_batch, z)
+                tqdm_.set_description("EPOCH: {}, D loss: {}, G loss: {}".format(epoch, d_loss, g_loss))
+
+            generated_images = 0.5 * self._generator.predict(np.random.normal(0, 1, (25, self.g_latent_dim))) + 0.5
+            sample_images(generated_images, epoch, 'images')
+
+
+if __name__ == '__main__':
+    (X_train, _), (_, _) = mnist.load_data()
+    X_train = X_train / 127.5 - 1.
+    X_train = np.expand_dims(X_train, axis=3)
+
+    gan = GAN(discriminator_layers_dims=[600, 300], generator_layers_dims=[300, 600, 784])
+    gan.fit(X_train, epochs=30)
---
+++ utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def dataIterator(data, batch_size):
+    """
+    From great jupyter notebook by Tim Sainburg:
+    http://github.com/timsainb/Tensorflow-MultiGPU-VAE-GAN
+    This contains some changes
+    Args:
+        data:
+        batch_size:
+
+    Returns:
+
+    """
+
+    while True:
+        length = data.shape[0] #
+        idxs = np.arange(0, length)
+        np.random.shuffle(idxs)
+        for batch_idx in range(0, length, batch_size):
+            if batch_idx + batch_size > length:
+                break
+            cur_idxs = idxs[batch_idx:batch_idx + batch_size]
+            batch = data[cur_idxs]
+            yield batch
+
+
+def sample_images(imgs, epoch, path):
+    r, c = 5, 5
+    fig, axs = plt.subplots(r, c)
+    cnt = 0
+    for i in range(r):
+        for j in range(c):
+            axs[i, j].imshow(imgs[cnt, :, :, 0], cmap='gray')
+            axs[i, j].axis('off')
+            cnt += 1
+    fig.savefig("images/%d.png" % epoch)
+    fig.savefig('{}\\{}.png'.format(path, epoch))
+    plt.close()
